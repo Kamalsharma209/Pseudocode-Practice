d7e087f9840d6e02588fae9bb0bbd27a0cc7c6ea
Your Name <Kamalsharma4769@gmail.com>
2025-07-31 13:47:40 +0530

    Doing Solution1

---
+++ src/Solution1.java
@@ -0,0 +1,15 @@
+public class Solution1 {
+    public static void main(String[] args) {
+        int z = 0;
+        int x = 5 , y= 3;
+        if(x>y){
+            if(y>0){
+                z=(x/y);
+            }
+            else{
+                z=x*y;
+            }
+        }
+        System.out.println(z);
+    }
+}
