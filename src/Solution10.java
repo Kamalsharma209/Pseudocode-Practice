73e9c96150d7aca01d1214b5ea09cd65afb360d2
Your Name <Kamalsharma4769@gmail.com>
2025-07-31 14:52:56 +0530

    Doing Solution 9 and Solution 10

---
+++ src/Solution10.java
@@ -0,0 +1,26 @@
+public class Solution10 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i <=3; i++) {
+            for (int j = 1; j <= 2; j++) {
+                count = count + 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
+//count = 0
+//for i from 1 to 3:
+//    for j from 1 to 2:
+//        count = count + 1
+//    end for
+//end for
+//Print count
+//```
+//**Options:**
+//- a) 2
+//- b) 3
+//- c) 6
+//- d) 5
+//
+//**Answer:** c
---
+++ src/Solution9.java
@@ -0,0 +1,26 @@
+public class Solution9 {
+    public static void main(String[] args) {
+        int value = 2;
+        int sum = 0;
+        while (value <= 10) {
+            sum += value;
+            value =  value+2;
+        }
+        System.out.println(sum);
+    }
+}
+//value = 2
+//sum = 0
+//while value <= 10:
+//    sum = sum + value
+//    value = value + 2
+//end while
+//Print sum
+//```
+//**Options:**
+//- a) 30
+//- b) 36
+//- c) 25
+//- d) 20
+//
+//**Answer:** a
