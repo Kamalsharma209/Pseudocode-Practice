642bb24b83776cef814d1850b16f3b7b1563e80a
Your Name <Kamalsharma4769@gmail.com>
2025-07-31 14:42:05 +0530

    Doing Solution6

---
+++ src/Solution6.java
@@ -0,0 +1,24 @@
+public class Solution6 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i=0; i<=5; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
+
+
+//sum = 0
+//for i from 1 to 5:
+//    sum = sum + i
+//end for
+//Print sum
+//```
+//**Options:**
+//- a) 10
+//- b) 15
+//- c) 5
+//- d) 0
+//
+//**Answer:** b
