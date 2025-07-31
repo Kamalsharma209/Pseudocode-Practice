cbeb9e866459e05520417ace78458004ab720bd7
Your Name <Kamalsharma4769@gmail.com>
2025-07-31 14:39:16 +0530

    Doing Solution5

---
+++ src/Solution5.java
@@ -0,0 +1,33 @@
+public class Solution5 {
+    public static void main(String[] args) {
+        int score = 85;
+        if(score >= 90){
+            System.out.println("A");
+        } else if (score >= 0) {
+            System.out.println("B");
+        } else {
+            System.out.println("C");
+        }
+    }
+}
+
+
+
+
+
+
+// Integer score
+//Set score = 85
+//if score >= 90 then
+//    Print "A"
+//else if score __ 80 then
+//    Print "B"
+//else
+//    Print "C"
+//end if
+//```
+//**Options:**
+//- a) =<
+//- b) <=
+//- c) >=
+//- d) =>
