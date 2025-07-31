5887d61ad02966ed103df5d549c6a34fd5e2d2ea
Your Name <Kamalsharma4769@gmail.com>
2025-07-31 14:07:17 +0530

    Doing Solution3

---
+++ src/Solution3.java
@@ -0,0 +1,15 @@
+public class Solution3 {
+    public static void main(String[] args) {
+        int n=2;
+        switch (n){
+            case 1:
+                System.out.println("One");
+            case  2:
+                System.out.println("Two");
+            case 3:
+                System.out.println("Three");
+            default:
+                System.out.println("OTHER");
+        }
+    }
+}
---
+++ src/Soluton2.java
@@ -0,0 +1,11 @@
+public class Soluton2 {
+    public static void main(String[] args) {
+        int x = 5;
+        if(x>0 && x%2 == 0){
+            System.out.println("POSITIVE ODD");
+        }
+        else{
+            System.out.println("OTHER");
+        }
+    }
+}
