7f3c99ed9a1c34bd817609adf1227a996e5385c1
Your Name <Kamalsharma4769@gmail.com>
2025-07-31 14:21:09 +0530

    Doing Solution4

---
+++ src/Solution4.java
@@ -0,0 +1,13 @@
+public class Solution4 {
+    public static void main(String[] args) {
+        int x =5;
+        int y=10;
+        if(!(x>0 && y<15)){
+            System.out.println(x+y);
+        }else{
+            System.out.println(x*y);
+        }
+    }
+}
+
+// ANSWER = 50
