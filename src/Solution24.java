b1e1d384047ace3e4544b96d067412ea2f97740f
Your Name <Kamalsharma4769@gmail.com>
2025-08-01 14:01:58 +0530

    Doing Solution 24 and 25

---
+++ src/Solution24.java
@@ -0,0 +1,20 @@
+public class Solution24 {
+    public static void main(String[] args) {
+        int a = 16;
+        int b= a >> 2+1;
+        System.out.println(b);
+    }
+}
+//### Question 24
+//```
+//a = 16
+//b = a >> 2 + 1
+//Print b
+//```
+//**Options:**
+//- a) 8
+//- b) 2
+//- c) 4
+//- d) 5
+//
+//**Answer:** b
---
+++ src/Solution25.java
@@ -0,0 +1,22 @@
+public class Solution25 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 6;
+       int  c=(a|b)>>1;
+        System.out.println(c);
+    }
+}
+//### Question 25
+//```
+//a = 4
+//b = 6
+//c = (a | b) >> 1
+//Print c
+//```
+//**Options:**
+//- a) 5
+//- b) 3
+//- c) 2
+//- d) 1
+//
+//**Answer:** b
