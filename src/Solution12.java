61c05ebe6ed14f04db3addabd6a2dfa9af6842f3
Your Name <Kamalsharma4769@gmail.com>
2025-08-01 13:24:01 +0530

    Doing Solution 12

---
+++ src/Solution12.java
@@ -0,0 +1,22 @@
+public class Solution12 {
+    public static void main(String[] args) {
+        int x=11, y=4;
+        int result = (x-y+121)%x;
+        System.out.println(result);
+    }
+}
+
+//### Question 12
+//```
+//Integer x, y, result
+//Set x = 11, y = 4
+//result = (x - y + 121) % x
+//Print result
+//```
+//**Options:**
+//- a) 5
+//- b) 6
+//- c) 7
+//- d) 8
+//
+//**Answer:** c
