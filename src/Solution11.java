b69615b21043e68a255b881282d26275828badd7
Your Name <Kamalsharma4769@gmail.com>
2025-08-01 13:21:23 +0530

    Doing Solution 11

---
+++ src/Solution11.java
@@ -0,0 +1,22 @@
+public class Solution11 {
+    public static void main(String[] args) {
+        int x=5, y=3 ;
+        int z = x+y*x-2;
+        System.out.println(z);
+    }
+}
+
+//### Question 11
+//```
+//x = 5
+//y = 3
+//z = x + y * x - 2
+//Print z
+//```
+//**Options:**
+//- a) 18
+//- b) 23
+//- c) 15
+//- d) 28
+//
+//**Answer:** a
