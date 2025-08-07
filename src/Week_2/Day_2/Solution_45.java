72c0e485c15939fb52344ccd5ffa75f5b06be900
Your Name <Kamalsharma4769@gmail.com>
2025-08-07 13:17:02 +0530

    Week_2 -- Day_2 (Solution_11 and Solution_12)

--- src/Week_2/Day_2/Solution_45.java
+++ src/Week_2/Day_2/Solution_45.java
@@ -1,4 +1,12 @@
 package Week_2.Day_2;
 
 public class Solution_45 {
+    public static int calc(int x) {
+        return x * x + 1;
+    }
+    public static void main(String[] args) {
+        System.out.println(calc(2) + calc(3));
+    }
 }
+
+// Answer = 15
---
+++ src/Week_2/Day_2/Solution_46.java
@@ -0,0 +1,12 @@
+package Week_2.Day_2;
+
+public class Solution_46 {
+    public static int f(int x) {
+        return x % 2 == 0 ? x : f(x - 1);
+    }
+    public static void main(String[] args) {
+        System.out.println(f(7));
+    }
+}
+
+// Answer = 6
