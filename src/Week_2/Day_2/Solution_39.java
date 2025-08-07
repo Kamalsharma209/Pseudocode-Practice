358cbe6d72dea56ef66f05016e076c9092426435
Your Name <Kamalsharma4769@gmail.com>
2025-08-07 13:05:37 +0530

    Week_2 -- Day_2 (Solution_4)

---
+++ src/Week_2/Day_2/Solution_39.java
@@ -0,0 +1,16 @@
+package Week_2.Day_2;
+
+public class Solution_39 {
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int result = mul(add(1, 2), add(2, 3));
+        System.out.println(result);
+    }
+}
+
+// Answer = 15
