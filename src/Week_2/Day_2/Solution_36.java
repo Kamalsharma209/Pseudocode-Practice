d798ac35952f8e62151b08fa571d02491f3954c0
Your Name <Kamalsharma4769@gmail.com>
2025-08-07 12:59:34 +0530

    Week_2 -- Day_2 (Solution_1)

---
+++ src/Week_2/Day_2/Solution_36.java
@@ -0,0 +1,15 @@
+package Week_2.Day_2;
+
+public class Solution_36 {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int x = add(add(2, 3), 4);
+        System.out.println(x);
+    }
+}
+
+
+
+// Answer = 9
